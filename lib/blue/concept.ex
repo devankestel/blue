678d928c45db8a73eccca2b5451d89f71ce59523
Devan Kestel <devan.kestel@gmail.com>
2022-12-28 12:17:06 -0500

    (feat): WIP add some typespecs.

--- lib/blue/concept.ex
+++ lib/blue/concept.ex
@@ -1,14 +1,19 @@
 defmodule Blue.Concept do
+    alias Blue.Concept
+
     defstruct [
         position: {0, 0}
     ]
     
-    def new(), do: __struct__
+    @spec new() :: %Concept{}
+    def new(), do: __struct__()
 
+    @spec move_avatar(%Concept{}, atom) :: %Concept{} 
     def move_avatar(avatar, direction) do
         %{avatar | position: move(avatar.position, direction)}
     end
 
+    @spec move({integer, integer}, atom) :: {integer, integer}
     def move(position, direction) do
         {x, y} = position
         case direction do

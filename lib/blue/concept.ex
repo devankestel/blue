1d0029152fdfc7463a767ee8b3f13a22a3a02069
Devan Kestel <devan.kestel@gmail.com>
2022-12-28 03:55:04 -0500

    (feat): WIP avatar position.

--- lib/blue/concept.ex
+++ lib/blue/concept.ex
@@ -4,4 +4,24 @@ defmodule Blue.Concept do
     ]
     
     def new(), do: __struct__
+
+    def move_avatar(avatar, direction) do
+        %{avatar | position: move(avatar.position, direction)}
+    end
+
+    def move(position, direction) do
+        {x, y} = position
+        case direction do
+            :up ->
+                {x, y+1}
+            :down -> 
+                {x, y-1}
+            :left -> 
+                {x-1, y}
+            :right -> 
+                {x+1, y}
+            _ -> 
+                {x, y} 
+        end
+    end
 end
--- test/blue/concept_test.ex
+++ test/blue/concept_test.ex
@@ -9,4 +9,37 @@ defmodule Blue.ConceptTest do
             assert avatar.position == {0, 0}
         end
     end
+
+    describe "move/2" do
+        test "moves up" do
+            position = {1, 1}
+            new_position = Concept.move(position, :up)
+            assert new_position == {1, 2}
+        end
+        test "moves down" do
+            position = {1, 1}
+            new_position = Concept.move(position, :down)
+            assert new_position == {1, 0} 
+        end
+        test "moves left" do
+            position = {1, 1}
+            new_position = Concept.move(position, :left)
+            assert new_position == {0, 1} 
+        end
+        test "moves right" do
+            position = {1, 1}
+            new_position = Concept.move(position, :right)
+            assert new_position == {2, 1}
+        end
+    end
+
+    describe "move_avatar/2" do
+        test "moves up" do
+            avatar = Concept.new()
+
+            new_avatar = Concept.move_avatar(avatar, :up)
+
+            assert new_avatar.position == {0, 1}
+        end
+    end
 end

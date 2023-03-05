7a0461bd9204dd6a74992b5e886d6c47465bf550
Devan Kestel <devan.kestel@gmail.com>
2023-03-05 14:35:50 -0500

    (feat): poor man's parameterization.

--- test/blue/action_test.exs
+++ test/blue/action_test.exs
@@ -123,41 +123,35 @@ defmodule Blue.ActionTest do
 
   describe "update_canvas_designer_mode/2" do
     test "it adds a blue item sprite" do
-      state = State.new()
-      canvas = Canvas.new()
-      protagonist_sprite = %Sprite{
-        type: :protagonist,
-        color: :black,
-        grid_coordinate: {1, 1}
-      }
-      canvas = %{canvas | sprites: [protagonist_sprite]}
-      designer_mode = DesignerMode.new()
-      buttons = %{designer_mode.buttons | add_blue_item_sprite: true}
-      designer_mode = %{designer_mode | on: true, buttons: buttons}
-      state = %{state | canvas: canvas, designer_mode: designer_mode}
-      svg_coordinate = {85, 85}
-
       true_button = :add_blue_item_sprite
       expected_sprite = %Sprite{
         type: :item,
         color: :blue,
         grid_coordinate: {5, 5}
       }
-      grid_coordinate = expected_sprite.grid_coordinate
-      type = expected_sprite.type
-      color = expected_sprite.color
 
-      expected_canvas = %{canvas | sprites: [protagonist_sprite, expected_sprite]}
+      test_update_canvas_designer_mode(true_button, expected_sprite)
+    end
+    test "it adds a red item sprite" do
+      true_button = :add_red_item_sprite
+      expected_sprite = %Sprite{
+        type: :item,
+        color: :red,
+        grid_coordinate: {5, 5}
+      }
 
-      patch(DesignerModeButtons, :get_true_button, true_button)
-      patch(Action, :add_sprite, expected_canvas)
+      test_update_canvas_designer_mode(true_button, expected_sprite)
+    end
+    test "it adds a wall sprite" do
+      true_button = :add_wall_sprite
+      expected_sprite = %Sprite{
+        type: :wall,
+        color: :gray,
+        grid_coordinate: {5, 5}
+      }
 
-      updated_canvas = Action.update_canvas_designer_mode(svg_coordinate, state)
-
-      assert updated_canvas == expected_canvas
-      assert_called DesignerModeButtons.get_true_button(buttons), 1
-      assert_called Action.add_sprite(grid_coordinate, type, color, canvas), 1
-      end
+      test_update_canvas_designer_mode(true_button, expected_sprite)
+    end
   end
 
   describe "add_protagonist_sprite/3" do
@@ -237,6 +231,43 @@ defmodule Blue.ActionTest do
     end
   end
 
+  def test_update_canvas_designer_mode(true_button, expected_sprite) do
+    state = State.new()
+    canvas = Canvas.new()
+    protagonist_sprite = %Sprite{
+      type: :protagonist,
+      color: :black,
+      grid_coordinate: {1, 1}
+    }
+    canvas = %{canvas | sprites: [protagonist_sprite]}
+    designer_mode = DesignerMode.new()
+    buttons = %{designer_mode.buttons | add_blue_item_sprite: true}
+    designer_mode = %{designer_mode | on: true, buttons: buttons}
+    state = %{state | canvas: canvas, designer_mode: designer_mode}
+    svg_coordinate = {85, 85}
+
+    true_button = :add_blue_item_sprite
+    expected_sprite = %Sprite{
+      type: :item,
+      color: :blue,
+      grid_coordinate: {5, 5}
+    }
+    grid_coordinate = expected_sprite.grid_coordinate
+    type = expected_sprite.type
+    color = expected_sprite.color
+
+    expected_canvas = %{canvas | sprites: [protagonist_sprite, expected_sprite]}
+
+    patch(DesignerModeButtons, :get_true_button, true_button)
+    patch(Action, :add_sprite, expected_canvas)
+
+    updated_canvas = Action.update_canvas_designer_mode(svg_coordinate, state)
+
+    assert updated_canvas == expected_canvas
+    assert_called DesignerModeButtons.get_true_button(buttons), 1
+    assert_called Action.add_sprite(grid_coordinate, type, color, canvas), 1
+  end
+
 
   def test_update_canvas(key_pressed, direction, expected_protagonist_grid_coordinate) do
     canvas = Canvas.new()
@@ -259,5 +290,4 @@ defmodule Blue.ActionTest do
     assert_called Direction.from_key_to_atom(key_pressed), 1
     assert_called Action.move_protagonist(direction, canvas), 1
   end
-
 end

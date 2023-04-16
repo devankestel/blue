f8c62588500a21595c43cd370857a1f4fdd935c2
Devan Kestel <devan.kestel@gmail.com>
2023-04-15 21:38:46 -0400

    (feat): WIP live_test with Floki

--- test/blue_web/live/live_test.exs
+++ test/blue_web/live/live_test.exs
@@ -15,16 +15,98 @@ defmodule BlueWeb.BlueLiveTest do
     end
   end
 
-  describe "click events?" do
+  describe "designer mode" do
     test "click on designer mode?", %{conn: conn} do
       {:ok, view, _html} = live(conn, "/blue")
 
 
-      html = view
-      |> element("button", "Designer mode")
+      element_result = view
+      |> element("#designer-mode-button", "Designer mode")
+      IO.inspect("element result")
+      IO.inspect(element_result)
+      html = element_result
       |> render_click()
 
       assert html =~ "Designer mode: true"
+      assert html =~ "Add protagonist sprite"
+      assert html =~ "Add red item sprite"
+      assert html =~ "Add blue item sprite"
+      assert html =~ "Add wall sprite"
+      assert html =~ "Delete sprite"
+
+    end
+
+    test "add a protagonist", %{conn: conn} do
+      #       In testing, the view is stateful/mutable, so you can do something like this:
+
+      # {:ok, view, _html} = live(conn, "/path")
+
+      # view |> element("#open-form-button") |> render_click
+
+      # view |> form("#my-form", %{foo: %{bar: "baz"}}) |> render_submit
+
+      # assert render(view) =~ "form was submitted!"
+
+
+      {:ok, view, _html} = live(conn, "/blue")
+
+
+      element_result = view
+      |> element("#designer-mode-button", "Designer mode")
+
+      html = element_result
+      |> render_click()
+
+      view
+      |> element("button", "protagonist")
+      |> render_click()
+
+      parsed_fragment =
+        view
+        |> render()
+        |> Floki.parse_fragment!()
+
+        # IO.inspect(parsed_fragment)
+        items =
+          parsed_fragment
+          |> Floki.find("#wall_sprite")
+
+        # @TODO: find count of each sprite on svg
+        # assert other props like location
+        # and maybe assert size?
+        # figure out how to do a click event
+        # and move the protagonist to collect a thing
+        # and assert it all over again
+        # This will definitely break a lot of unit tests
+        # throughout the app.
+
+
+        # IO.inspect(items)
+
+        items_count =
+          items
+            |> length()
+
+        IO.inspect("Wall count:")
+        IO.inspect(items_count)
+          # |> Enum.map(&Floki.text(&1, sep: " "))
+
+      assert items_count == 80
+      assert items == ["item 1", "item 2"]
+
+
+      # # export html to doc
+      # File.write("lib/blue/rendered_html_test.html", html)
+      # # export result to doc
+      # File.write("lib/blue/rendered_element_result_test.html", IO.inspect(result))
+
+      # assert html =~ "Designer mode: true"
+      # assert html =~ "Add protagonist sprite"
+      # assert html =~ "Add red item sprite"
+      # assert html =~ "Add blue item sprite"
+      # assert html =~ "Add wall sprite"
+      # assert html =~ "Delete sprite"
+
     end
   end
 

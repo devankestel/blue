5db72784839d2f9f5798d7d478e543d732000091
Devan Kestel <devan.kestel@gmail.com>
2023-08-03 21:34:57 -0400

    (feat): sprite parsing on fleek.

--- test/blue_web/live/live_test.exs
+++ test/blue_web/live/live_test.exs
@@ -6,7 +6,7 @@ defmodule BlueWeb.BlueLiveTest do
   alias BlueWeb.BlueLive
   alias Blue.{Canvas, Sprite}
 
-  defp parse_wall_sprites(
+  defp parse_sprites(
     {
         "svg",
         [
@@ -30,6 +30,25 @@ defmodule BlueWeb.BlueLiveTest do
         viewbox: viewbox 
     }
   end
+
+  defp get_sprite_props_and_count(parsed_fragment, sprite_id) do
+    sprite_props =
+            parsed_fragment
+            |> Floki.find(sprite_id)
+            |> Enum.map(&parse_sprites(&1))
+
+    IO.inspect("#{sprite_id} Found:")
+    IO.inspect(sprite_props)
+
+    sprites_count =
+        sprite_props
+        |> length()
+
+    IO.inspect("#{sprite_id} count:")
+    IO.inspect(sprites_count)
+    {sprite_props, sprites_count}
+  end
+
 
   defp wall_sprite_assertions(wall_sprite) do
     assert wall_sprite.id == "wall_sprite"
@@ -69,52 +88,39 @@ defmodule BlueWeb.BlueLiveTest do
     end
 
     test "add a protagonist", %{conn: conn} do
-      # In testing, the view is stateful/mutable, so you can do something like this:
+        # In testing, the view is stateful/mutable, so you can do something like this:
 
-      # {:ok, view, _html} = live(conn, "/path")
+        # {:ok, view, _html} = live(conn, "/path")
 
-      # view |> element("#open-form-button") |> render_click
+        # view |> element("#open-form-button") |> render_click
 
-      # view |> form("#my-form", %{foo: %{bar: "baz"}}) |> render_submit
+        # view |> form("#my-form", %{foo: %{bar: "baz"}}) |> render_submit
 
-      # assert render(view) =~ "form was submitted!"
-
-
-      {:ok, view, _html} = live(conn, "/blue")
+        # assert render(view) =~ "form was submitted!"
 
 
-      element_result = view
-      |> element("#designer-mode-button", "Designer mode")
+        {:ok, view, _html} = live(conn, "/blue")
 
-      html = element_result
-      |> render_click()
 
-      view
-      |> element("button", "protagonist")
-      |> render_click()
+        element_result = view
+            |> element("#designer-mode-button", "Designer mode")
 
-      parsed_fragment =
+        html = element_result
+            |> render_click()
+
         view
-        |> render()
-        |> Floki.parse_fragment!()
+            |> element("button", "protagonist")
+            |> render_click()
+
+        parsed_fragment =
+            view
+            |> render()
+            |> Floki.parse_fragment!()
 
         # IO.inspect(parsed_fragment)
         wall_sprites =
-          parsed_fragment
-          |> Floki.find("#wall_sprite")
-
-
-        # @TODO:
-        # Fix the git security stuff [X]
-        # find count of each sprite on svg
-        # assert other props like location
-        # and maybe assert size?
-        # figure out how to do a click event
-        # and move the protagonist to collect a thing
-        # and assert it all over again
-        # This will definitely break a lot of unit tests
-        # throughout the app.
-
+            parsed_fragment
+            |> Floki.find("#wall_sprite")
 
         # IO.inspect(items)
 
@@ -128,7 +134,7 @@ defmodule BlueWeb.BlueLiveTest do
     
         parsed_wall_sprites = 
             wall_sprites
-            |> Enum.map(&parse_wall_sprites(&1))
+            |> Enum.map(&parse_sprites(&1))
         IO.inspect("parsed wall sprites")
         IO.inspect(parsed_wall_sprites)
 
@@ -136,29 +142,33 @@ defmodule BlueWeb.BlueLiveTest do
         second_wall_sprite = Enum.at(parsed_wall_sprites, 1)
         third_wall_sprite = Enum.at(parsed_wall_sprites, 2)
 
-      assert wall_sprites_count == 3
-      assert first_wall_sprite.x == "100"
-      assert first_wall_sprite.y == "150"
-      assert second_wall_sprite.x == "100"
-      assert second_wall_sprite.y == "200"
-      assert third_wall_sprite.x == "100"
-      assert third_wall_sprite.y == "250"
+        assert wall_sprites_count == 3
+        assert first_wall_sprite.x == "100"
+        assert first_wall_sprite.y == "150"
+        assert second_wall_sprite.x == "100"
+        assert second_wall_sprite.y == "200"
+        assert third_wall_sprite.x == "100"
+        assert third_wall_sprite.y == "250"
       
 
-      parsed_wall_sprites
-        |> Enum.each(& wall_sprite_assertions(&1))
+        parsed_wall_sprites
+            |> Enum.each(& wall_sprite_assertions(&1))
 
-      # # export html to doc
-      # File.write("lib/blue/rendered_html_test.html", html)
-      # # export result to doc
-      # File.write("lib/blue/rendered_element_result_test.html", IO.inspect(result))
+        {lemon_sprites_props, lemon_sprites_count} = get_sprite_props_and_count(parsed_fragment, "#lemon_sprite")
 
-      # assert html =~ "Designer mode: true"
-      # assert html =~ "Add protagonist sprite"
-      # assert html =~ "Add red item sprite"
-      # assert html =~ "Add blue item sprite"
-      # assert html =~ "Add wall sprite"
-      # assert html =~ "Delete sprite"
+        assert lemon_sprites_count == 2
+
+        # # export html to doc
+        # File.write("lib/blue/rendered_html_test.html", html)
+        # # export result to doc
+        # File.write("lib/blue/rendered_element_result_test.html", IO.inspect(result))
+
+        # assert html =~ "Designer mode: true"
+        # assert html =~ "Add protagonist sprite"
+        # assert html =~ "Add red item sprite"
+        # assert html =~ "Add blue item sprite"
+        # assert html =~ "Add wall sprite"
+        # assert html =~ "Delete sprite"
 
     end
   end
